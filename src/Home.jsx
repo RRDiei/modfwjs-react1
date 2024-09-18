67bdd6fdf8cb9ee3fe0f1e268dde91701d2c6b9e
Diego García Esteban <diegoges@protonmail.com>
2024-09-18 21:41:37 +0100

    añade enlaces a cada película

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import "./Home.css";
 
 export default function Home(props) {
-  return <div className="Home-container">{props.peliculas}</div>;
+  return <div className="Home-container mt-5">{props.peliculas}</div>;
 }
--- src/Pelicula.jsx
+++ src/Pelicula.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Pelicula(props) {
   const img = `https://image.tmdb.org/t/p/w220_and_h330_face${props.pelicula.poster_path}`;
   const descripcion = `${props.pelicula.overview.slice(0, 150)}...`;
+  const cardUrl = props.pelicula.id.toString() ;
   return (
     <div className="card" style={{ width: "18rem" }}>
       <img src={img} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{props.pelicula.title}</h5>
         <p className="card-text">{descripcion}</p>
-        <button className="btn btn-primary">+ INFO</button>
+        <Link to={cardUrl} className="btn btn-primary">
+          + INFO
+        </Link>
       </div>
     </div>
   );
